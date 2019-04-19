df84878da11accdfdfb6b28318c3a2d6a56250cd
Ryan Christoffersen <ryan@ryanchristo.com>
2019-04-19 22:24:21 +0200

    Add react foundation

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => (
+  <div className={styles.container}>
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        {'scrupl'}
+      </h1>
+    </header>
+  </div>
+);
+
+export default App;
