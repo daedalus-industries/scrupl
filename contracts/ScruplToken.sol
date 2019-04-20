668fca7dcce10bef09ff8a33e2cc825bdaca5ff2
gichiba <griffin@colony.io>
2019-04-20 13:56:47 +0200

    add erc20 scrupl token

---
+++ contracts/ScruplToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol"
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol"
+
+contract ScruplToken is ERC20Mintable {
+
+    function indulgence(uint256 value) public {
+        _burn(msg.sender, value);
+    }
+
+}
