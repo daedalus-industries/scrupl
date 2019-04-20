fc19fd4bf7f012c991d583392a66d1b21221eb23
gichiba <griffin@colony.io>
2019-04-20 16:47:20 +0200

    crappy implementation of burn function

--- contracts/ScruplBox.sol
+++ contracts/ScruplBox.sol
@@ -4,4 +4,13 @@ import "lib/budgetBox/contracts/BudgetBox.sol";
 
 contract ScruplBox is BudgetBox {
 
+  function addVote(uint256 vote) public {
+    revert("restricted in scruplbox to those who burn scruples");
+  }
+
+  function indulgence(uint256 amount, uint256 vote) payable public {
+    voteArray.push(vote);
+    if (voteArray.length >= MAX_VOTES) loadVotesToBBoxStorage();
+  }
+
 }
